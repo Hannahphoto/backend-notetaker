e0865391d8c9c58f3801760cd7cf74c4e497b4c5
Hannahphoto <hannahprice67@gmail.com>
2023-12-14 22:14:06 -0500

    everything finally working

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const htmlRoute = require('./routes/index');
 const path = require('path');
 const apiRoute = require('./routes/notes');
-const db = require('./db/db.json');
-const fs = require('fs');
+let db = require('./db/db.json');
+const fs = require('fs/promises');
+const { error } = require('console');
 
 
 const app = express();
@@ -13,18 +14,18 @@ const PORT = process.env.PORT || 3001;
 
 //Middleware for parsing JSON and urlencoded from data.
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 // app.use('/api', apiRoute);
 // app.use('/', htmlRoute);
 app.use(express.static(path.join(__dirname, 'public')));
 
-function uniqueID(max){
+function uniqueID(max) {
     return Math.floor(Math.random() * max) + 1;
 };
 
 //GET notes 
 app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/db/db.json'))
+    res.json(db);
 });
 
 app.get('/', (req, res) =>
@@ -35,44 +36,45 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.post('/api/notes', (req, res)=>{
+app.post('/api/notes', async (req, res) => {
     let note = req.body;
     note['id'] = uniqueID(1000);
-    
-    fs.readFile('./db/db.json', ( err, res) =>{
-        if(err) console.log("Error: ",err);
 
-        let data = JSON.parse(res);
-        data.push(note);
+    const data = JSON.parse(await fs.readFile('./db/db.json'));
+    data.push(note);
+    db.push(note);
 
-        fs.writeFile('./db/db.json', JSON.stringify(data), err => {
-            if (err) throw err;
-            console.log('Data has been saved');
-        })
+    try {
+        await fs.writeFile('./db/db.json', JSON.stringify(data));
+        console.log("post made")
+        return res.status(201).json(note)
 
-    })
-    res.redirect('/notes');
-
+    } catch (err) {
+        console.log(err);
+        res.status(500).json("NOT GOOD")
+    }
 });
 
-// DELETE route for notes
-app.delete('/api/notes/:id', (req, res)=>{
-    const noteDelete = req.params.note.id;
-    fs.readFile('./db/db.json')
-    console.log('read file')
-    .then((data)=> JSON.parse(data))
-    .then((json) => {
-       const update = json.filter((note) => note.id !==noteDelete);
-       return fs.writeToFile('./db/db.json', JSON.stringify(update), err =>{
-        if(err) throw err;
-        console.log('Note deleted.');
-       });
-    })
 
-    .catch((err)=>{
-        console/log(err);
-        res.status(500).json({error: 'Internal Server Error.'})
-    });
+
+
+
+// DELETE route for notes
+app.delete('/api/notes/:id', async (req, res) => {
+    const noteDelete = parseFloat(req.params.id);
+    console.log(noteDelete);
+    const savedNotes = JSON.parse(await fs.readFile('./db/db.json', 'utf8'));
+    console.log(savedNotes)
+    db = savedNotes.filter((note) => note.id !== noteDelete);
+
+    try {
+        await fs.writeFile('./db/db.json', JSON.stringify(db));
+        console.log('Note deleted')
+        return res.status(200).json("ok")
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: 'Internal Server Error.' })
+    };
 });
 
 
